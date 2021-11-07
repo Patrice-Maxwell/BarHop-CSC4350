aedec02de1cbb8a5db86986b5b1fbf875eff7467
Gayoung Kim <60451182+kyoung224@users.noreply.github.com>
2021-11-06 22:38:52 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function addNewInput(){
-  var newInputBox = document.createElement('div');
-  newInputBox.innerHTML = "<input type = 'datetime-local' id = 'new' name = 'availability'>";
-  document.getElementById("newInputBox").appendChild(newInputBox);
-}
